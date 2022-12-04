f34f8ed6cbb9f5a9a838382be63f5db99551ea51
Gmdbelema <bjhoodbelema@gmail.com>
2022-12-04 10:08:24 -0800

    A C function that prints some basic info about Python lists.

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,12 +1,12 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <Python.h>
 
 /**
- ** print_python_list_info - prints python list info
-  *
-  * @p: PyObject
-  * Return: no return
-  */
+ * print_python_list_info - prints python list info
+ *
+ * @p: PyObject
+ * Return: no return
+ */
 void print_python_list_info(PyObject *p)
 {
 	long int size, i;
